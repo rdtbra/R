38aa5c3432c3e5418dbcf1bf324ef6f2c89a3d55
rdtbra <reinaldo@adtinformatica.com.br>
2023-07-02 20:52:27 -0300

    RDT: 20230702 - Ponto de entrada.

--- src/main/Rmain.c
+++ src/main/Rmain.c
@@ -22,6 +22,7 @@ int Rf_initialize_R(int ac, char **av); /* in ../unix/system.c */
 
 #include <Rinterface.h>
 
+/* RDT: 20230702 - Ponto de entrada. */
 int main(int ac, char **av)
 {
     R_running_as_main_program = 1;
